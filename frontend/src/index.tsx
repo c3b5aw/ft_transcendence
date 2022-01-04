4281bd2558470b512adfd1fa803a659fc999b95c
eoliveir <eoliveir42@gmail.com>
2022-01-04 14:59:14 +0100

    test commit and push

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom'
+import { Route, Routes } from 'react-router-dom'
+
+// import Home from './pages/Home';
+// import ConnectionPage from './pages/ConnectionPage';
+
+function Survey() {
+    return (
+        <div>
+            <h1>Survey 🧮</h1>
+        </div>
+    )
+}
+
+function HomeConnection() {
+    return (
+        <div>
+            <h1>Home 🧮</h1>
+        </div>
+    )
+}
+
+function TestReturn() {
+	return (
+		<Router>
+			<Routes>
+				<Route path="/" element={<HomeConnection />}/>
+				<Route path="/connection" element={<Survey />}/>
+			</Routes>
+		</Router>	
+	);
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+      {TestReturn()}
+    </React.StrictMode>,
+    document.getElementById('root')
 );
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     {/* <Router> */}
+//         <Home />
+//     {/* </Router> */}
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// );
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ConnectionPage from './ConnectionPage';
+import "./App.css";
+
+const Home = () => {
+	return (
+		<div className="Home">
+			<ConnectionPage />
+		</div>
+	);
+};
+
+export default Home;
