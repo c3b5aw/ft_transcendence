4de8a7392912c7010a5ed067949af3f482a6add6
c3b5aw <dev@c3b5aw.dev>
2022-01-07 16:21:18 +0100

    added health route

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
 
-	@Get()
-	getHello(): string {
-		return this.appService.getHello();
+	@ApiResponse({ status: 200, description: 'The service is up and running' })
+	@Get('health')
+	getHealth(): string {
+		return 'OK';
 	}
+
 }
