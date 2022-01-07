2d75084a68ee2d557f995e0a6873983f00f15369
c3b5aw <dev@c3b5aw.dev>
2022-01-07 16:21:40 +0100

    db connected

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,31 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
+
+import { AuthModule } from './auth/auth.module'
 
 @Module({
-	imports: [],
+	imports: [
+		TypeOrmModule.forRoot({
+			// https://docs.nestjs.com/techniques/database
+			type: 'postgres',
+			host: process.env.POSTGRES_HOST,
+			port: Number(process.env.POSTGRES_PORT),
+			username: process.env.POSTGRES_USER,
+			password: process.env.POSTGRES_PASSWORD,
+			database: process.env.POSTGRES_DB,
+
+			/*
+				Entites auto solve: 
+				https://docs.nestjs.com/techniques/database#async-configuration
+			*/
+			entities: [ __dirname + '/*/entities/*.entity.ts' ],
+			
+			synchronize: true,
+		}),
+		AuthModule
+	],
 	controllers: [AppController],
-	providers: [AppService],
+	providers: [],
 })
 export class AppModule {}
