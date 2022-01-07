5d4529b7a8d450b82764c732e6be7e63593e3de3
c3b5aw <dev@c3b5aw.dev>
2022-01-07 16:21:06 +0100

    added session modules, auto gen docs

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,11 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+import * as session from 'express-session'
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 
 	app.setGlobalPrefix('api');
+	app.use(
+		session({
+			secret: process.env.SESSION_SECRET || process.env.BACKEND_SESSION_SECRET,
+			resave: false,
+			saveUninitialized: false,
+		})
+	)
 
-	await app.listen(3000);
+	if (process.env.NODE_ENV !== 'production') {
+		const config = new DocumentBuilder()
+			.setTitle('ft_transcendence API')
+			.setDescription('')
+			.setVersion('1.0')
+			.addTag('')
+			.build();
+		const document = SwaggerModule.createDocument(app, config);
+		SwaggerModule.setup('docs', app, document);
+	}
+
+	await app.listen(80);
 }
 bootstrap();
