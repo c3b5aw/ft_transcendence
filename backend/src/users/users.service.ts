04cac52b98b5ede671d9775dc128f8cda7106112
c3b5aw <dev@c3b5aw.dev>
2022-01-08 16:36:18 +0100

    add comment about user repository

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -5,6 +5,9 @@ import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
+	/*
+		view https://docs.nestjs.com/techniques/database#custom-repository
+	*/
 	constructor(@InjectRepository(User) private userRepository: Repository<User>) {}
 
 	async createUser(userDetails: User) : Promise<User> {
