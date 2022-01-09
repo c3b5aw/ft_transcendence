5a9cc652ea4f1e8271b35ba2bda6e60bab650d90
c3b5aw <dev@c3b5aw.dev>
2022-01-09 19:32:25 +0100

    update 42 callback

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,5 +1,5 @@
-import { Controller, Get, Req, Res, UseGuards } from '@nestjs/common';
-import { ApiResponse, ApiTags, ApiCreatedResponse } from '@nestjs/swagger';
+import { Controller, Get, Header, Redirect, Req, Res, UseGuards } from '@nestjs/common';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 import { Session } from 'express-session';
 
 import { Intra42Guard } from './guards/intra42.guard';
@@ -10,24 +10,40 @@ export class AuthController {
 
 	@Get('login')
 	@UseGuards(Intra42Guard)
-	async login(@Req() req: any) {
+	async login() {}
+
+	@Get('status')
+	@Header('Content-Type', 'application/json')
+	status(@Req() req: any): string {
+		if (req.isAuthenticated()) {
+			return JSON.stringify({
+				isAuthenticated: true,
+				user: {
+					id: req.user.id,
+					login: req.user.login,
+				}
+			});
+		}
+		return JSON.stringify({
+			isAuthenticated: false,
+		});
 	}
 
-	@Get('redirect')
+	@Get('42/callback')
 	@UseGuards(Intra42Guard)
-	redirect(@Req() req: any): string {
-		if (req.user) {
-			return "Logged as " + req.user.displayName;
-		} else {
-			return "Not logged";
-		}
-	}
+	@Redirect('/api/auth/status', 301)
+	redirect() {}
 
 	@Get('logout')
+	@Header('Content-Type', 'application/json')
 	@UseGuards(Intra42Guard)
 	@ApiResponse({ status: 200, description: 'User session has been logged out.' })
 	logout(@Req() req: { session: Session }): string {
-		req.session.destroy(() => {});
-		return "OK";
+		if (req.session) {
+			req.session.destroy(() => {});
+		}
+		return JSON.stringify({
+			status: "OK"
+		});
 	}
 }
