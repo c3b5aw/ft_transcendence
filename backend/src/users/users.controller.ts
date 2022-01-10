9b979b64b266aa4ca708d4145e444524469e9f47
c3b5aw <dev@c3b5aw.dev>
2022-01-10 23:00:28 +0100

    add guard users controller

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,34 +1,41 @@
-import { Controller, Get } from "@nestjs/common";
-import { ApiTags } from "@nestjs/swagger";
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
 
 @ApiTags('users')
 @Controller('users')
 export class UserController {
 	
 	@Get('/')
+	@UseGuards(JwtGuard)
 	getUsers() {}
 
 	@Get('/:id')
+	@UseGuards(JwtGuard)
 	getUser() {}
 
 	@Get('/:id/stats')
+	@UseGuards(JwtGuard)
 	getUserStats() {}
 
 	@Get('/:id/rank')
+	@UseGuards(JwtGuard)
 	getUserRank() {}
 
-	// @Get('api/matchs/id')
-	// @Get('api/matchs/)
-
 	@Get('/:id/matchs')
+	@UseGuards(JwtGuard)
 	getUserMatchs() {}
 
 	@Get('/:id/friends')
+	@UseGuards(JwtGuard)
 	getUserFriens() {}
 
 	@Get('/:id/avatar')
+	@UseGuards(JwtGuard)
 	getUserAvatar() {}
 
 	@Get('/:id/achievements')
+	@UseGuards(JwtGuard)
 	getUserAchievements() {}
 }
