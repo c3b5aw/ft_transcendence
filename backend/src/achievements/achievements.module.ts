0f7c344ff1cc2b8bcd3c047a711d41363d47c657
c3b5aw <dev@c3b5aw.dev>
2022-01-12 16:49:06 +0100

    enable achievemnts module

--- backend/src/achievements/achievements.module.ts
+++ backend/src/achievements/achievements.module.ts
@@ -4,13 +4,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Achievement } from './entities/achievement.entity';
 import { UserAchievement } from './entities/user_achievements.entity';
 import { AchievementsService } from './achievements.service';
+import { AchievementsController } from './achievements.controller';
 
 @Module({
 	imports: [
 		TypeOrmModule.forFeature([Achievement]),
 		TypeOrmModule.forFeature([UserAchievement]),
 	],
-	controllers: [],
+	controllers: [
+		AchievementsController,
+	],
 	providers: [
 		AchievementsService,
 	],
