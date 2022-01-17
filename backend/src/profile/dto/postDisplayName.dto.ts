754f6a57e0b2b67afa11b9d1005f0029637ec9bd
c3b5aw <dev@c3b5aw.dev>
2022-01-17 19:22:22 +0100

    update displayname dto

--- backend/src/profile/dto/postDisplayName.dto.ts
+++ backend/src/profile/dto/postDisplayName.dto.ts
@@ -1,3 +1,8 @@
-export interface PostDisplayNameDto {
+import { IsString, MinLength, MaxLength } from 'class-validator';
+
+export class PostDisplayNameDto {
+	@IsString()
+	@MinLength(3)
+	@MaxLength(64)
 	display_name: string;
 }
