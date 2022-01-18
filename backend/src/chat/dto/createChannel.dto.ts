f0471683e691e9afaf8cf698e07f1b590a35acda
c3b5aw <dev@c3b5aw.dev>
2022-01-18 15:00:08 +0100

    add api property

--- backend/src/chat/dto/createChannel.dto.ts
+++ backend/src/chat/dto/createChannel.dto.ts
@@ -1,13 +1,16 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, MinLength, MaxLength } from 'class-validator';
 
 export class CreateChannelDto {
 	@IsString()
 	@MinLength(3)
 	@MaxLength(64)
+	@ApiProperty()
 	name: string;
 
 	@IsString()
 	@MinLength(0)
 	@MaxLength(64)
+	@ApiProperty()
 	password: string;
 }
--- backend/src/chat/dto/updateChannelPassword.dto.ts
+++ backend/src/chat/dto/updateChannelPassword.dto.ts
@@ -1,8 +1,10 @@
 import { IsString, MinLength, MaxLength } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateChannelPasswordDto {
 	@IsString()
 	@MinLength(0)
 	@MaxLength(64)
+	@ApiProperty()
 	password: string;
 }
