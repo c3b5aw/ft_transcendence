b788ab95f1c426a5db600ce383ecdc5865015256
c3b5aw <dev@c3b5aw.dev>
2022-01-24 17:13:54 +0100

    update matchmaking gateway

--- backend/src/matchmaking/matchmaking.gateway.ts
+++ backend/src/matchmaking/matchmaking.gateway.ts
@@ -2,10 +2,13 @@ import { Logger, UseGuards } from '@nestjs/common';
 import { OnGatewayConnection, OnGatewayDisconnect,
 		WebSocketGateway, WebSocketServer,
 		SubscribeMessage } from '@nestjs/websockets';
+import { Server } from 'socket.io';
 
-import { Server, Socket } from 'socket.io';
-import { JwtTwoFactorGuard } from 'src/2fa/guards/2fa.guard';
 import { WsGuard } from 'src/auth/guards/ws.guard';
+import { WSClient } from 'src/ws/datastructures/wsclient.struct';
+import { MatchmakingService } from './matchmaking.service';
+
+import { User } from 'src/users/entities/user.entity';
 
 @UseGuards(WsGuard)
 @WebSocketGateway({ namespace: '/matchmaking', cors: { origin: '*'} })
@@ -14,25 +17,32 @@ export class MatchmakingGateway implements OnGatewayConnection, OnGatewayDisconn
 	@WebSocketServer() server: Server;
 	private logger: Logger = new Logger('MatchmakingGateway');
 
-	constructor() {}
+	constructor(private readonly matchMakingService: MatchmakingService) {}
 
 	afterInit() {
 		this.logger.log('MatchmakingGateway initialized');
+		this.matchMakingService.setServer(this.server);
 	}
 
-	async handleConnection(client: Socket) {
-		// verify TK
-		// check user status
-		// add in matchmaking
+	async handleConnection(client: WSClient) {
+		const user: User = await this.matchMakingService.verifyUser(client);
+		if (!user)
+			return;
+
+		client.emit('onSuccess', { message: 'welcome to the matchmaking server' });
 	}
 
-	async handleDisconnect(client: Socket) {
-		// remove from matchmaking		
+	async handleDisconnect(client: WSClient) {
+		console.log(client.user);
+		// console.log('disconnect');
+		// if (!client.user)
+		// 	console.log('disconnect');
+		// remove from matchmaking
 	}
 
 	@SubscribeMessage('matchmaking::request')
-	async matchmakingRequest(client: Socket, data: any) {
-		console.log(client);
+	async matchmakingRequest(client: WSClient, body: any) {
+		console.log(client.user);
 		// verify JSON
 
 		// check match type
@@ -41,12 +51,12 @@ export class MatchmakingGateway implements OnGatewayConnection, OnGatewayDisconn
 	}
 
 	@SubscribeMessage('matchmaking::cancel')
-	async matchmakingCancel(client: Socket, data: any) {
+	async matchmakingCancel(client: WSClient, body: any) {
 		// remove from queue
 	}
 
 	@SubscribeMessage('matchmaking::accept')
-	async matchmakingAccept(client: Socket, data: any) {
+	async matchmakingAccept(client: WSClient, body: any) {
 		// remove from queue and redirect to game
 	}
 }
