91ca2a85634b1bf329eae74c3f109ce496f89cc3
c3b5aw <dev@c3b5aw.dev>
2022-01-24 19:22:25 +0100

    game gateway user validation setup

--- backend/src/game/game.gateway.ts
+++ backend/src/game/game.gateway.ts
@@ -7,15 +7,20 @@ import { Server } from 'socket.io';
 import { WsGuard } from 'src/ws/guards/ws.guard';
 import { WSClient } from 'src/ws/datastructures/wsclient.struct';
 
+import { GameService } from './game.service';
+
 @UseGuards(WsGuard)
 @WebSocketGateway({ namespace: '/game', cors: { origin: '*'} })
 export class GameGateway implements OnGatewayConnection, OnGatewayDisconnect {
+
+	constructor(private readonly gameService: GameService) {}
 
 	@WebSocketServer() server: Server;
 	private logger: Logger = new Logger('GameGateway');
 
 	afterInit() {
 		this.logger.log('GameGateway initialized');
+		this.gameService.setServer(this.server);
 	}
 
 	async handleConnection(client: WSClient) {}
--- backend/src/game/game.module.ts
+++ backend/src/game/game.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+
+import { UsersModule } from 'src/users/users.module';
 
 import { GameService } from './game.service';
 
 @Module({
-	imports: [],
+	imports: [
+		JwtModule.register({
+			secret: process.env.JWT_SECRET,
+			signOptions: { 
+				expiresIn: Number(process.env.JWT_EXPIRATION) 
+			},
+		}),
+		UsersModule
+	],
 	controllers: [],
 	providers: [ GameService ],
 	exports: [],
