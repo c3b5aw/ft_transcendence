557691b53a23071919c8e1a1c54669179e061f24
c3b5aw <dev@c3b5aw.dev>
2022-01-24 18:19:18 +0100

    update mm system

--- backend/src/matchmaking/matchmaking.gateway.ts
+++ backend/src/matchmaking/matchmaking.gateway.ts
@@ -9,10 +9,12 @@ import { WSClient } from 'src/ws/datastructures/wsclient.struct';
 import { MatchmakingService } from './matchmaking.service';
 
 import { User } from 'src/users/entities/user.entity';
+import { MatchType } from 'src/matchs/entities/types.enum';
+import { WS_parse } from 'src/ws/parse';
 
 @UseGuards(WsGuard)
 @WebSocketGateway({ namespace: '/matchmaking', cors: { origin: '*'} })
-export class MatchmakingGateway implements OnGatewayConnection, OnGatewayDisconnect {
+export class MatchmakingGateway implements OnGatewayConnection {
 
 	@WebSocketServer() server: Server;
 	private logger: Logger = new Logger('MatchmakingGateway');
@@ -32,31 +34,24 @@ export class MatchmakingGateway implements OnGatewayConnection, OnGatewayDisconn
 		client.emit('onSuccess', { message: 'welcome to the matchmaking server' });
 	}
 
-	async handleDisconnect(client: WSClient) {
-		console.log(client.user);
-		// console.log('disconnect');
-		// if (!client.user)
-		// 	console.log('disconnect');
-		// remove from matchmaking
+	@SubscribeMessage('matchmaking::join')
+	async matchmakingJoin(client: WSClient, body: string) {
+		const { match_type, room } = await WS_parse(body);
+
+		if (!match_type)
+			return client.emit('onError', `match_type is required`);
+		if (match_type === MatchType.MATCH_NORMAL && !room)
+			return client.emit('onError', `opponent is required`);
+
+		if (match_type === MatchType.MATCH_NORMAL) {
+			await this.matchMakingService.joinNormalQueue(client, room);
+		} else if (match_type === MatchType.MATCH_RANKED) {
+			await this.matchMakingService.joinRankedQueue(client);
+		}
 	}
 
-	@SubscribeMessage('matchmaking::request')
-	async matchmakingRequest(client: WSClient, body: any) {
-		console.log(client.user);
-		// verify JSON
-
-		// check match type
-		// add in queue
-		client.emit('ok', "message");
-	}
-
-	@SubscribeMessage('matchmaking::cancel')
-	async matchmakingCancel(client: WSClient, body: any) {
-		// remove from queue
-	}
-
-	@SubscribeMessage('matchmaking::accept')
-	async matchmakingAccept(client: WSClient, body: any) {
-		// remove from queue and redirect to game
+	@SubscribeMessage('matchmaking::leave')
+	async matchmakingLeave(client: WSClient) {
+		await this.matchMakingService.leaveQueue(client);
 	}
 }
--- backend/src/matchmaking/matchmaking.service.ts
+++ backend/src/matchmaking/matchmaking.service.ts
@@ -2,7 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 
 import { Server } from 'socket.io';
+
 import { WsGuard } from 'src/auth/guards/ws.guard';
+
 import { UsersService } from 'src/users/users.service';
 
 import { WSClient } from 'src/ws/datastructures/wsclient.struct';
@@ -11,7 +13,11 @@ import { WSClient } from 'src/ws/datastructures/wsclient.struct';
 export class MatchmakingService {
 
 	constructor(private readonly usersService: UsersService,
-		private readonly jwtService: JwtService) {}
+		private readonly jwtService: JwtService)
+	{
+		global.mm_clients = {};
+		global.queues = {};
+	}
 
 	server: Server;
 
@@ -21,5 +27,77 @@ export class MatchmakingService {
 
 	async verifyUser(client: WSClient) {
 		return WsGuard.verify(client, this.jwtService, this.usersService);
+	}
+
+	/*
+		QUEUES
+	*/
+
+	async joinQueue(client: WSClient, room: string, queueType: string = 'NORMAL') {
+		if (global.mm_clients[client.user.id])
+			return client.emit('onError', { error: `already in a queue` });
+
+		client.join(room);
+
+		global.mm_clients[client.user.id] = room;
+		global.queues[room].push(client);
+		client.emit('onSuccess', { message: `${queueType} queue joined` });
+	
+		this.queueUpdate(room);
+	}
+
+	async leaveQueue(client: WSClient) {
+		const queue = global.client[client.user.id];
+		if (!queue)
+			return client.emit('onError', { error: `not in a queue` });
+
+		client.leave(queue);
+		global.queues[queue].splice(global.queues[queue].indexOf(client), 1);
+		delete global.mm_clients[client.user.id];
+
+		return client.emit('onSuccess', { message: `queue left` });
+	}
+
+	async joinNormalQueue(client: WSClient, queueName: string) {
+		const room: string = `#MM-NORMAL-${queueName}`;
+
+		return this.joinQueue(client, room);
+	}
+
+	async joinRankedQueue(client: WSClient) {
+		const { elo } = await this.usersService.getStatsById(client.user.id);
+		if (!elo)
+			return client.emit('onError', { error: `unable to fetch elo` });
+
+		const roundedElo = Math.round(elo / 100) * 100;
+		const roomElo = (roundedElo / 100) % 2 === 0 ? roundedElo - 100 : roundedElo;
+
+		const room = `#MM-RANKED-${roomElo}`;
+
+		return this.joinQueue(client, room, 'RANKED');
+	}
+
+	async queueUpdate(room: string) {
+		if (!global.queues[room])
+			return;
+
+		const queue = global.queues[room];
+		if (queue.length < 2)
+			return;
+
+		// ToDo: await create new Game 
+		const match = {
+			id: Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15),
+		};
+
+		// Send match to room
+		this.server.to(room).emit('matchmaking::onFound', { match });
+
+		// remove from queue and clients
+		const [user1, user2] = queue;
+		global.queues[room] = {};
+
+		delete global.mm_clients[user1];
+		delete global.mm_clients[user2];
 	}
 }
