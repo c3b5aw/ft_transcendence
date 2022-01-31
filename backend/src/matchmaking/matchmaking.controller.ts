87d48fb62b15f8323b12f216906eb1cbb587186a
c3b5aw <dev@c3b5aw.dev>
2022-01-31 21:49:10 +0100

    get rooms endpoint

---
+++ backend/src/matchmaking/matchmaking.controller.ts
@@ -0,0 +1,20 @@
+import { Controller, UseGuards, Get, Header } from '@nestjs/common';
+import { ApiCookieAuth, ApiTags, ApiOperation } from '@nestjs/swagger';
+
+import { JwtTwoFactorGuard } from 'src/2fa/guards/2fa.guard';
+import { MatchmakingService } from './matchmaking.service';
+
+@ApiTags('matchmaking')
+@ApiCookieAuth()
+@UseGuards(JwtTwoFactorGuard)
+@Controller('matchmaking')
+export class MatchmakingController {
+	constructor(private readonly matchmakingServie: MatchmakingService) {}
+
+	@Get('rooms')
+	@Header('Content-Type', 'application/json')
+	@ApiOperation({ summary: 'Get all rooms' })
+	async getRooms() {
+		return this.matchmakingServie.getRooms();
+	}
+}
