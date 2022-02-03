a8d6536c31fabf91671c6d4f44f0e92018661a2a
c3b5aw <dev@c3b5aw.dev>
2022-02-03 16:28:05 +0100

    update channel join with null pass

--- backend/src/chat/chat.gateway.ts
+++ backend/src/chat/chat.gateway.ts
@@ -78,9 +78,9 @@ export class ChatGateway implements OnGatewayConnection, OnGatewayDisconnect {
 			return client.emit('onError', { error: WsError.CHANNEL_IS_TUNNEL });
 
 		const hash = await this.chatService.getChannelPasswordHash(channel.id);
-		if (hash === undefined || hash === null)
+		if (hash === undefined)
 			return client.emit('onError', { error: WsError.UNABLE_AUTH_CHANNEL });
-		else if (hash === "")
+		else if (hash === "" || hash === null)
 			return await this.chatService.wsJoinChannel(client, channel, true);
 
 		const pwd = createHash('md5').update(payload.password).digest('hex');
