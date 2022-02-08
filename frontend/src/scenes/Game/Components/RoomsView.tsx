6e8495ac2fc84276ce55c0752e368265309bd222
eoliveir <eoliveir42@gmail.com>
2022-02-08 16:02:42 +0100

    delete unused funtions and comments

--- frontend/src/scenes/Game/Components/RoomsView.tsx
+++ frontend/src/scenes/Game/Components/RoomsView.tsx
@@ -40,7 +40,7 @@ function RoomsView() {
 					autoHideDuration: 3000,
 				});
 			}
-		}, 1000)
+		}, 2000)
 		return () => clearInterval(interval);
 	// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
@@ -91,40 +91,6 @@ function RoomsView() {
 		>
             <MyAppBarClose setOpen={handleClose} name={PAGE.MENU_PARTIE}/>
 			<DialogContent>
-				{/* <Stack
-					direction="row"
-					sx={{width: 1, marginBottom: "15px"}}
-					justifyContent="space-between"
-					alignItems="center"
-					spacing={2}
-				>
-					<Button
-						variant="contained"
-						color="success"
-						disabled={disabledButton()}
-						onClick={() => setOpenCreateRoom(true)}
-					>
-						<Typography
-							variant="h6"
-							style={{fontFamily: "Myriad Pro", color: disabledButton() ? "grey" : "white"}}
-						>
-							Create new room
-						</Typography>
-					</Button>
-					<Button
-						variant="contained"
-						color="success"
-						disabled={disabledButton()}
-						onClick={() => setOpenMatchMaking(true)}
-					>
-						<Typography
-							variant="h6"
-							style={{fontFamily: "Myriad Pro", color: disabledButton() ? "grey" : "white"}}
-						>
-							Rechercher une partie classée
-						</Typography>
-					</Button>
-				</Stack> */}
 				<Stack sx={{backgroundColor: 'white', height: 1, width: 1, borderRadius: 5}}>
 					<TableContainer sx={{borderRadius: 5}}>
 						<Table>
