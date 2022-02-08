c2dd8f1b321b7b6ead6c7613bfdd0ad6d73091dd
eoliveir <eoliveir42@gmail.com>
2022-02-08 21:41:53 +0100

    add volume buttons

--- frontend/src/scenes/Game/GameButtons.tsx
+++ frontend/src/scenes/Game/GameButtons.tsx
@@ -1,4 +1,6 @@
-import { Button, Stack, Typography } from '@mui/material';
+import { Button, IconButton, Stack, Typography } from '@mui/material';
+import VolumeMuteIcon from '@mui/icons-material/VolumeMute';
+import VolumeUpIcon from '@mui/icons-material/VolumeUp';
 
 export default function GameButtons(props: any) {
 
@@ -9,27 +11,37 @@ export default function GameButtons(props: any) {
 	}
 
 	return (
-		<Stack direction="row" justifyContent="center" alignItems="center" spacing={{xs: 1, sm: 5}}>
-			<Button
-				variant="contained"
-				color="success"
-				onClick={props.backgroundCallback}
-			>
-				<Typography>Color</Typography>
-			</Button>
-			<Button
-				variant="contained"
-				color="warning"
-				onClick = {handlePause}
-			>
-				<Typography>Pause</Typography>
-			</Button>
-			<Button
-				variant="contained"
-				color="error"
-			>
-				<Typography>Quitter</Typography>
-			</Button>
+		<Stack direction="row" justifyContent="space-around" sx={{width: 1}}>
+			<Stack direction="row" justifyContent="center" alignItems="center" spacing={{xs: 1, sm: 5}}>
+				<Button
+					variant="contained"
+					color="success"
+					onClick={props.backgroundCallback}
+				>
+					<Typography>Color</Typography>
+				</Button>
+				<Button
+					variant="contained"
+					color="warning"
+					onClick = {handlePause}
+				>
+					<Typography>Pause</Typography>
+				</Button>
+				<Button
+					variant="contained"
+					color="error"
+				>
+					<Typography>Quitter</Typography>
+				</Button>
+			</Stack>
+			<Stack direction="row" spacing={3}>
+				<IconButton>
+					<VolumeUpIcon />
+				</IconButton>
+				<IconButton>
+					<VolumeMuteIcon />
+				</IconButton>
+			</Stack>
 		</Stack>
 	)
 }
