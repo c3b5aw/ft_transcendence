6641adafc7a0d06ba0f79c2fb12ee2670b1fe222
eoliveir <eoliveir42@gmail.com>
2022-02-09 18:41:31 +0100

    update conflicts 3

--- backend/src/game/objects/game.player.class.ts
+++ backend/src/game/objects/game.player.class.ts
@@ -1,58 +1,59 @@
 import { GAME_PLAYER_START_Y, GAME_PLAYER_SPEED, GAME_BORDER_SIZE,
-		GAME_PLAYER_HEIGHT, GAME_CANVAS_HEIGHT } from './game.constants';
+	GAME_PLAYER_HEIGHT, GAME_CANVAS_HEIGHT } from './game.constants';
 import { GameMoves } from './moves.enums';
 
 export class GamePlayer {
-	public id: number;
-	public login: string = "";
-	public slot: number;
+public id: number;
+public login: string = "";
+public slot: number;
 
-	public ingame: boolean = false;
+public ingame: boolean = false;
 
-	public y: number;
+public y: number;
 
-	public move: GameMoves;
+public move: GameMoves;
 
-	public score: number;
+public score: number;
 
-	constructor(id: number, slot: number) {
-		this.id = id;
-		this.slot = slot;
-		
-		this.score = 0;
-		this.reset();
+constructor(id: number, login: string, slot: number) {
+	this.id = id;
+	this.login = login;
+	this.slot = slot;
+	
+	this.score = 0;
+	this.reset();
+}
+
+public __repr__() {
+	return {
+		id: this.id,
+		login: this.login,
+		slot: this.slot,
+		y: this.y,
+		move: this.move,
+		score: this.score
 	}
+}
 
-	public __repr__() {
-		return {
-			id: this.id,
-			login: this.login,
-			slot: this.slot,
-			y: this.y,
-			move: this.move,
-			score: this.score
-		}
-	}
+public reset() {
+	this.y = GAME_PLAYER_START_Y;
+	this.move = GameMoves.MOVE_STOP;
+}
 
-	public reset() {
-		this.y = GAME_PLAYER_START_Y;
-		this.move = GameMoves.MOVE_STOP;
-	}
+public registerMove(move: GameMoves) {
+	this.move = move;
+}
 
-	public registerMove(move: GameMoves) {
-		this.move = move;
-	}
+public update() {
+	if (this.move === GameMoves.MOVE_UP)
+		this.y -= GAME_PLAYER_SPEED;
+	else if (this.move === GameMoves.MOVE_DOWN)
+		this.y += GAME_PLAYER_SPEED;
 
-	public update() {
-		if (this.move === GameMoves.MOVE_UP)
-			this.y -= GAME_PLAYER_SPEED;
-		else if (this.move === GameMoves.MOVE_DOWN)
-			this.y += GAME_PLAYER_SPEED;
+	if (this.y < GAME_BORDER_SIZE)
+		this.y = GAME_BORDER_SIZE;
 
-		if (this.y < GAME_BORDER_SIZE)
-			this.y = GAME_BORDER_SIZE;
-
-		if (this.y + GAME_PLAYER_HEIGHT > GAME_CANVAS_HEIGHT - GAME_BORDER_SIZE)
-			this.y = GAME_CANVAS_HEIGHT - GAME_BORDER_SIZE - GAME_PLAYER_HEIGHT;
-	}
+	if (this.y + GAME_PLAYER_HEIGHT > GAME_CANVAS_HEIGHT - GAME_BORDER_SIZE)
+		this.y = GAME_CANVAS_HEIGHT - GAME_BORDER_SIZE - GAME_PLAYER_HEIGHT;
+}
 }
